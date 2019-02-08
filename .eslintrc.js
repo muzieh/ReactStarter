bc7e42a6abd8b3d7de687d15dc6c7b28f0a082d9
Marcin Owerczuk <muzieh@gmail.com>
2019-02-08 18:18:23 +0000

    babel webpack react eslint express

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+      "no-console": "off",
+      "linebreak-style": ["error","windows"],
+		"indent": ["error", "tab"],
+		"no-tabs": ["error", {"allowIndentationTabs": true}],
+		"react/jsx-indent":["error", "tab"],
+		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    },
+	"env": {
+		"browser": true,
+		"node": true,
+	},
+};
